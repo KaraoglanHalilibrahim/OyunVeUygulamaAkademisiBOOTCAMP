fda64cabec136f7eeb4b69e12ca06b3d232deafc
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-06-06 10:48:09 +0300

    parkour mekaniği deneme-01

--- OUA_Bootcamp/Assets/Scenes/Halilibrahim/scripts/parkourcreator.cs
+++ OUA_Bootcamp/Assets/Scenes/Halilibrahim/scripts/parkourcreator.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class parkourcreator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform parkurNesnesi;
+    public float rastgeleKonumAralýðý = 5f;
+    public float hareketHizi = 2f;
+
+    private Vector3[] baslangicKonumlari;
+    private bool kaydiriliyor = false;
+    private float kaymaZamani = 0f;
+
     void Start()
     {
         OyunuBaþlat();
     }
 
+    void Update()
+    {
+        if (kaydiriliyor)
+        {
+            kaymaZamani += Time.deltaTime * hareketHizi;
+
+            for (int i = 0; i < parkurNesnesi.childCount; i++)
+            {
+                Transform obje = parkurNesnesi.GetChild(i);
+                Vector3 hedefKonum = baslangicKonumlari[i];
+                Vector3 yeniKonum = Vector3.Lerp(obje.position, hedefKonum, kaymaZamani);
+                obje.position = yeniKonum;
+            }
+
+            if (kaymaZamani >= 1f)
+            {
+                kaydiriliyor = false;
+            }
+        }
+    }
+
     void OyunuBaþlat()
     {
-        for (int i = 0; i < parkurNesnesi.childCount; i++)
+        int objeSayisi = parkurNesnesi.childCount;
+        baslangicKonumlari = new Vector3[objeSayisi];
+
+        // Baþlangýç konumlarýný kaydet
+        for (int i = 0; i < objeSayisi; i++)
+        {
+            baslangicKonumlari[i] = parkurNesnesi.GetChild(i).position;
+        }
+
+        // Objeleri rastgele konumlara yerleþtir
+        for (int i = 0; i < objeSayisi; i++)
         {
             Transform obje = parkurNesnesi.GetChild(i);
 
@@ -22,8 +60,18 @@ public class parkourcreator : MonoBehaviour
                                                 Random.Range(-rastgeleKonumAralýðý, rastgeleKonumAralýðý));
 
             // Objeyi rastgele konuma taþý
-            obje.position = obje.position + rastgeleKonum;
+            obje.position = baslangicKonumlari[i] + rastgeleKonum;
         }
+
+        kaydiriliyor = true;
+        kaymaZamani = 0f;
+    }
+
+    public void Resetle()
+    {
+        kaydiriliyor = true;
+        kaymaZamani = 0f;
     }
 }
+
 
