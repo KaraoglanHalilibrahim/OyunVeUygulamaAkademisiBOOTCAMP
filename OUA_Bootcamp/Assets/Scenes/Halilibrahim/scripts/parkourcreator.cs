10593ccf4282ad13eb5df148b6a8ee10698aef72
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-06-06 14:09:22 +0300

    Bazı_Düzeltmeler

--- OUA_Bootcamp/Assets/Scenes/Halilibrahim/scripts/parkourcreator.cs
+++ OUA_Bootcamp/Assets/Scenes/Halilibrahim/scripts/parkourcreator.cs
@@ -57,7 +57,7 @@ public class parkourcreator : MonoBehaviour
         for (int i = 0; i < parkurNesnesi.childCount; i++)
         {
             Transform obje = parkurNesnesi.GetChild(i);
-            Vector3 rastgeleKonum = new Vector3(Random.Range(-10f, 10f), alttaGelmeYuksekligi, Random.Range(-10f, 10f));
+            Vector3 rastgeleKonum = new Vector3(Random.Range(-10f, 100f), alttaGelmeYuksekligi, Random.Range(-10f, 100f));
             obje.position = rastgeleKonum;
         }
 
