98ade1ba8a85f1c322e56986df001027b38fec93
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-06-11 19:34:42 +0300

    .

--- OUA-Bootcamp/Assets/Halilibrahim/scripts/enemy_death.cs
+++ OUA-Bootcamp/Assets/Halilibrahim/scripts/enemy_death.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class enemy_death : MonoBehaviour
 {
     public Animator animator; // Animator bileþeni referansý
-    public bool death = false; // Ölüm durumu
+    private bool isDead = false; // Ölüm durumu
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Bullet")) // Çarpýþma objesi "Bullet" tag'ine sahipse
+        if (collision.gameObject.CompareTag("Bullet")) // Çarpýþan obje "Bullet" tag'ine sahipse
         {
-            death = true; // Ölüm durumunu true yap
-            animator.SetBool("death", death); // Animator'deki "death" parametresine ölüm durumunu atar
+            isDead = true; // Ölüm durumunu true yap
+            animator.SetBool("death", isDead); // Animator'deki "death" parametresine ölüm durumunu atar
         }
     }
 }
