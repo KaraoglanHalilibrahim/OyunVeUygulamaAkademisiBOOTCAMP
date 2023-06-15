2bafc0f5d78d7a1b748319d7c902b9e74a7456ac
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-06-15 16:08:58 +0300

    düşman_hit_reaksiyonları_düzeltildi

--- OUA-Bootcamp/Assets/Halilibrahim/Enemy01/Scripts/Enemypatrol.cs
+++ OUA-Bootcamp/Assets/Halilibrahim/Enemy01/Scripts/Enemypatrol.cs
@@ -6,8 +6,8 @@ public class Enemypatrol : MonoBehaviour
     public Transform target;
     public float minChaseSpeed = 3f;
     public float maxChaseSpeed = 5f;
-    public float delayBeforeMovement = 1.0f;
-    public float patrolStopDuration = 1.0f;
+    public float delayBeforeMovement = 1.3f;
+    public float patrolStopDuration = 1.3f;
 
     private Animator animator;
     private NavMeshAgent agent;
@@ -78,6 +78,8 @@ public class Enemypatrol : MonoBehaviour
     void StopMovement()
     {
         agent.isStopped = true;
+        float speed = 0f;
+        animator.SetFloat("Speed", speed);
     }
 
     void ResumePatrolling()
